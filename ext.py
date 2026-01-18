00f451802527f82a9d054a7cc2d6de0d476736bf
shotikoberoshvili <shotiko.beroshvilite06@geolab.edu.ge>
2026-01-18 20:20:43 +0000

    free4

--- ext.py
+++ ext.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
-from groq import Groq
-from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'shotiko'
@@ -13,4 +12,3 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
 load_dotenv()
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
