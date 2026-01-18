11890ae2db6c9d2542d87bf5169ddb9c01dd0230
shotikoberoshvili <shotiko.beroshvilite06@geolab.edu.ge>
2026-01-18 20:39:45 +0000

    ფრე

--- ext.py
+++ ext.py
@@ -1,14 +1,12 @@
+# ext.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 import os
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'shotiko'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "secret")
 
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
 
 
