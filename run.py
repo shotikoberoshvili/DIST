651762ff4ebbb0e299fb64f4d432df9a67e8c099
shotikoberoshvili <shotiko.beroshvilite06@geolab.edu.ge>
2026-01-18 21:33:00 +0000

    sd

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from ext import app
 from routes import delete, home, about, login, logout, register, calendar, account, profile, add_mentor, edit_mentor, tutors, detailed, rate_mentor
+from ext import app
 
 app.run(host="0.0.0.0")
