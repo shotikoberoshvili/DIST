114e3fbcf558a3e4e253aa68257cd231d93365b8
shotikoberoshvili <shotiko.beroshvilite06@geolab.edu.ge>
2026-01-18 20:56:13 +0000

    as

--- ext.py
+++ ext.py
@@ -1,16 +1,23 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
+# Database
 DATABASE_URL = os.environ.get("DATABASE_URL")
-
 if DATABASE_URL:
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/database.db"
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "secret")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Extensions
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
