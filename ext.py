1f9457293998fa541c2f21d0ae2c9ac4472508de
shotikoberoshvili <shotiko.beroshvilite06@geolab.edu.ge>
2026-01-18 20:51:05 +0000

    სდ

--- ext.py
+++ ext.py
@@ -1,12 +1,16 @@
-# ext.py
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "secret")
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/database.db"
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "secret")
 
 db = SQLAlchemy(app)
-
-
